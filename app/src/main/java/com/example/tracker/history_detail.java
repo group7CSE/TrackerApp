2acac022192f3d360a64d2501110132a2a260350
dheepiga <dheepigaraja@gmail.com>
2020-03-13 21:31:41 +0530

    database code modification2

---
+++ app/src/main/java/com/example/tracker/history_detail.java
@@ -0,0 +1,19 @@
+package com.example.tracker;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+@IgnoreExtraProperties
+public class history_detail {
+    public String RollNo;
+    public String ID;
+    public String MAC;
+    public String Date;
+    public String Time;
+
+    public history_detail(String roll_No, String Id, String mac, String date, String time) {
+        this.RollNo = roll_No;
+        this.ID = Id;
+        this.MAC = mac;
+        this.Date = date;
+        this.Time = time;
+    }
+}
